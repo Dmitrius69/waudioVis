80307a10085e4d42888b02f4cf937c7898f738ea
Dmitry Ivanov <dmitrius69@gmail.com>
2019-03-06 14:06:43 +0300

    добавлена начальная работа со звуковыми файлами

--- waudioVis/waudioVis/Form1.Designer.cs
+++ waudioVis/waudioVis/Form1.Designer.cs
@@ -30,20 +30,24 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.statToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.oPenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.fader1 = new NAudio.Gui.Fader();
+            this.waveViewer1 = new NAudio.Gui.WaveViewer();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.waveViewer1 = new NAudio.Gui.WaveViewer();
-            this.fader1 = new NAudio.Gui.Fader();
+            this.fnamelabel = new System.Windows.Forms.Label();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,10 +68,30 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.oPenToolStripMenuItem,
             this.toolStripMenuItem1,
-            this.testToolStripMenuItem});
+            this.testToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.exitToolStripMenuItem1});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // oPenToolStripMenuItem
+            // 
+            this.oPenToolStripMenuItem.Name = "oPenToolStripMenuItem";
+            this.oPenToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.oPenToolStripMenuItem.Text = "Open";
+            this.oPenToolStripMenuItem.Click += new System.EventHandler(this.oPenToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(213, 6);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.testToolStripMenuItem.Text = "Test";
             // 
             // statToolStripMenuItem
             // 
@@ -86,25 +110,8 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(108, 26);
             this.exitToolStripMenuItem.Text = "Exit";
-            // 
-            // oPenToolStripMenuItem
-            // 
-            this.oPenToolStripMenuItem.Name = "oPenToolStripMenuItem";
-            this.oPenToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.oPenToolStripMenuItem.Text = "Open";
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(213, 6);
-            // 
-            // testToolStripMenuItem
-            // 
-            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
-            this.testToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.testToolStripMenuItem.Text = "Test";
             // 
             // groupBox1
             // 
@@ -116,6 +123,31 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
+            // 
+            // fader1
+            // 
+            this.fader1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.fader1.Location = new System.Drawing.Point(382, 234);
+            this.fader1.Maximum = 0;
+            this.fader1.Minimum = 0;
+            this.fader1.Name = "fader1";
+            this.fader1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.fader1.Size = new System.Drawing.Size(324, 69);
+            this.fader1.TabIndex = 1;
+            this.fader1.Text = "fader1";
+            this.fader1.Value = -2147483648;
+            // 
+            // waveViewer1
+            // 
+            this.waveViewer1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.waveViewer1.ForeColor = System.Drawing.SystemColors.Info;
+            this.waveViewer1.Location = new System.Drawing.Point(0, 78);
+            this.waveViewer1.Name = "waveViewer1";
+            this.waveViewer1.SamplesPerPixel = 128;
+            this.waveViewer1.Size = new System.Drawing.Size(706, 150);
+            this.waveViewer1.StartPosition = ((long)(0));
+            this.waveViewer1.TabIndex = 0;
+            this.waveViewer1.WaveStream = null;
             // 
             // groupBox2
             // 
@@ -129,35 +161,33 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.fnamelabel);
             this.panel1.Location = new System.Drawing.Point(6, 21);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(363, 358);
             this.panel1.TabIndex = 0;
             // 
-            // waveViewer1
+            // fnamelabel
             // 
-            this.waveViewer1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.waveViewer1.ForeColor = System.Drawing.SystemColors.Info;
-            this.waveViewer1.Location = new System.Drawing.Point(0, 41);
-            this.waveViewer1.Name = "waveViewer1";
-            this.waveViewer1.SamplesPerPixel = 128;
-            this.waveViewer1.Size = new System.Drawing.Size(706, 150);
-            this.waveViewer1.StartPosition = ((long)(0));
-            this.waveViewer1.TabIndex = 0;
-            this.waveViewer1.WaveStream = null;
+            this.fnamelabel.AutoSize = true;
+            this.fnamelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fnamelabel.Location = new System.Drawing.Point(12, 19);
+            this.fnamelabel.Name = "fnamelabel";
+            this.fnamelabel.Size = new System.Drawing.Size(78, 24);
+            this.fnamelabel.TabIndex = 0;
+            this.fnamelabel.Text = "FName";
             // 
-            // fader1
+            // toolStripMenuItem3
             // 
-            this.fader1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.fader1.Location = new System.Drawing.Point(369, 234);
-            this.fader1.Maximum = 0;
-            this.fader1.Minimum = 0;
-            this.fader1.Name = "fader1";
-            this.fader1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            this.fader1.Size = new System.Drawing.Size(324, 69);
-            this.fader1.TabIndex = 1;
-            this.fader1.Text = "fader1";
-            this.fader1.Value = 0;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(213, 6);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
             // 
             // Form1
             // 
@@ -174,6 +204,8 @@
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,6 +226,9 @@
         private NAudio.Gui.WaveViewer waveViewer1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label fnamelabel;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
     }
 }
 
--- waudioVis/waudioVis/Form1.cs
+++ waudioVis/waudioVis/Form1.cs
@@ -16,10 +16,39 @@ namespace waudioVis
 {
     public partial class Form1 : Form
     {
+        NAudio.Wave.Mp3FileReader mpFile;
+        NAudio.Wave.WaveOut wvOut;
+
         public Form1()
         {
             InitializeComponent();
-           
+            wvOut = new NAudio.Wave.WaveOut();
+        }
+
+        private void oPenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int cntByte;
+            byte[] sampleBuffer = new byte[4096];
+
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.ShowDialog();
+
+            if (opf.FileName != null)
+            {
+                fnamelabel.Text = opf.FileName;
+                //wvOut.Init();
+                mpFile = new NAudio.Wave.Mp3FileReader(opf.FileName); //открываем файл
+                do
+                {
+                    cntByte = mpFile.Read(sampleBuffer, 0, sampleBuffer.Length);
+                    
+                } while (cntByte != 0); //считываем mp3 файл
+            }
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
